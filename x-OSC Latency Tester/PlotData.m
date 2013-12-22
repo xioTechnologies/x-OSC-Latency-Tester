be451404d240726bad42179be10a56865e5f95ef
xioTechnologies <social@x-io.co.uk>
2013-12-22 23:21:53 +0000

    Remove WiFi disconnections from analysis

--- x-OSC Latency Tester/PlotData.m
+++ x-OSC Latency Tester/PlotData.m
@@ -23,6 +23,17 @@ function distribution = PlotData(varargin)
     data = csvread(filePath);
     data = data * 1000;	% convert to ms
     
+    % Ignore repearated failed (i.e. WiFi disconecnt)
+    failed = (data > failThresh);
+    ignore = zeros(numel(data), 1);
+   	threshold = 3;     % assume discooencted 5 loop failures in a row
+    for i = threshold:numel(data)
+        if(failed((i-(threshold-1)):i) == ones(threshold,1))
+            ignore((i-(threshold-1)):i) = ones(threshold,1);
+        end
+    end
+    data = data(~ignore);
+    
     % Format data
     fails = nnz(data > failThresh);
     data = data(data < failThresh);
@@ -32,10 +43,10 @@ function distribution = PlotData(varargin)
     [p n e] = fileparts(filePath);
     disp(sprintf(strcat('\n', n, e, ' summary:')));
     disp(sprintf('Number of samples:\t%d', numel(data)));
-    disp(sprintf('Loop failures:\t\t%d', fails));
-    disp(sprintf('Mean latency:\t\t%f ms', mean(data)));
-    disp(sprintf('99%% less than:\t\t%f ms', data(round(length(data)*0.99))));
-    disp(sprintf('Worst case:\t\t\t%f ms', data(end)));
+    disp(sprintf('Loop failures:\t\t%0.1f%%', 100 * (fails / numel(data))));
+    disp(sprintf('Mean latency:\t\t%0.2f ms', mean(data)));
+    disp(sprintf('99%% less than:\t\t%0.2f ms', data(round(length(data)*0.99))));
+    disp(sprintf('Worst case:\t\t\t%0.2f ms', data(end)));
 
     % Plot distribution
     edges = floor(min(data)):width:ceil(max(data));
@@ -43,7 +54,6 @@ function distribution = PlotData(varargin)
     bar(edges + (width / 2), dist / sum(dist), 'LineStyle', lineStyle, 'FaceColor', faceColor, 'barwidth', 1);
     xlabel('Closed-loop latency (ms)');
     ylabel('Normalised number of samples');
-    title(sprintf('Distribution of %d samples', numel(data)));
 
     distribution = data;
 end
